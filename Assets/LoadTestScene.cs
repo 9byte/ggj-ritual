b257c28268d381a98d0ad90bf8168a2bec2d605e
kasoban <git@kasoban.de>
2016-01-29 22:12:07 -0800

    Initial project checkin

---
+++ Assets/LoadTestScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadTestScene : MonoBehaviour {
+
+	public void ClickTest (string input)
+	{
+		SceneManager.LoadScene(input);
+	}
+}
