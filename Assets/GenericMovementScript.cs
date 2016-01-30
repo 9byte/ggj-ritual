961d507b62c17a285c06decdd42b9c97aeed9f94
kasoban <git@kasoban.de>
2016-01-29 23:11:13 -0800

    movement test scene

---
+++ Assets/GenericMovementScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericMovementScript : MonoBehaviour {
+
+    public float xAccel = 1.0f;
+    public float yAccel = 1.0f;
+    public float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {	
+	}
+
+	// Update is called once per frame
+	void Update () {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 move = new Vector3((horizontal*xAccel), (vertical*yAccel), 0);
+     	transform.position += move * speed * Time.deltaTime;
+	}
+}
