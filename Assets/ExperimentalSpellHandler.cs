be10a2f6cc76404067534948cb2b7595b9f17ed0
Matthias Lücke <ml@9bt.de>
2016-01-30 16:27:55 +0100

    Moved ui stack handling to own component

--- Assets/ExperimentalSpellHandler.cs
+++ Assets/ExperimentalSpellHandler.cs
@@ -5,50 +5,40 @@ using UnityEngine.UI;
 
 public class ExperimentalSpellHandler : MonoBehaviour {
 
-	private enum SpellPress {
+	public enum SpellPress {
 		SPELL_0,
 		SPELL_1,
 		SPELL_2,
 		SPELL_3
 	}
-
-    private Text uiText;
-
-	private List<SpellPress> spellStack = new List<SpellPress>();
+        
+	public List<SpellPress> spellStack = new List<SpellPress>();
 
 	// Use this for initialization
 	void Start () {
-        uiText = GameObject.Find("UIStackBody").GetComponent<Text>();
-        uiText.text = "Empty! Summon something";
+      
 
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
+
 		if (Input.GetButtonDown ("Spell_0")) {
 			spellStack.Add(SpellPress.SPELL_0);
 			print("Added spell 0 to stack. Size: " + spellStack.Count);
-
-            uiText.text += "Spell 0 \n";
 		}
 		if (Input.GetButtonDown ("Spell_1")) {
 			spellStack.Add(SpellPress.SPELL_1);
 			print("Added spell 1 to stack. Size: " + spellStack.Count);
-
-            uiText.text += "Spell 1 \n";
 		}
 		if (Input.GetButtonDown ("Spell_2")) {
 			spellStack.Add(SpellPress.SPELL_2);
-			print("Added spell 2 to stack. Size: " + spellStack.Count);
-
-            uiText.text += "Spell 2 \n";
+            print("Added spell 2 to stack. Size: " + spellStack.Count);
 		}
 		if (Input.GetButtonDown ("Spell_3")) {
 			spellStack.Add(SpellPress.SPELL_3);
 			print("Added spell 3 to stack. Size: " + spellStack.Count);
-
-            uiText.text += "Spell 3 \n";
 		}
 	}
 }
---
+++ Assets/UIStack.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIStack : MonoBehaviour
+{
+    public GameObject spellHandlerObject;
+
+    private ExperimentalSpellHandler spellHandler;
+    private Text textStack;
+
+	void Start () 
+    {
+        //spellHandler = GameObject.Find("Capsule").GetComponent<ExperimentalSpellHandler>();
+        spellHandler = spellHandlerObject.GetComponent<ExperimentalSpellHandler>();
+        textStack = gameObject.GetComponent<Text>();
+	}
+	
+    void Update () 
+    {
+
+        textStack.text = "";
+        foreach (ExperimentalSpellHandler.SpellPress spell in spellHandler.spellStack)
+        {
+            textStack.text += spell.ToString() + "\n";
+        }
+	}
+}
