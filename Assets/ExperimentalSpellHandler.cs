39b7cc3f59feeeff24e3497b6d6a4b6bd55532ef
Matthias Lücke <ml@9bt.de>
2016-01-30 15:38:23 +0100

    Fixed SpellHandler and added spell stack ui

--- Assets/ExperimentalSpellHandler.cs
+++ Assets/ExperimentalSpellHandler.cs
@@ -18,8 +18,8 @@ public class ExperimentalSpellHandler : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-        uiText = GameObject.Find("UIStackBody").GetComponent<Text>;
-        uiText.text = "wtf!";
+        uiText = GameObject.Find("UIStackBody").GetComponent<Text>();
+        uiText.text = "Empty! Summon something";
 
 	}
 	
@@ -29,18 +29,26 @@ public class ExperimentalSpellHandler : MonoBehaviour {
 		if (Input.GetButtonDown ("Spell_0")) {
 			spellStack.Add(SpellPress.SPELL_0);
 			print("Added spell 0 to stack. Size: " + spellStack.Count);
+
+            uiText.text += "Spell 0 \n";
 		}
 		if (Input.GetButtonDown ("Spell_1")) {
 			spellStack.Add(SpellPress.SPELL_1);
 			print("Added spell 1 to stack. Size: " + spellStack.Count);
+
+            uiText.text += "Spell 1 \n";
 		}
 		if (Input.GetButtonDown ("Spell_2")) {
 			spellStack.Add(SpellPress.SPELL_2);
 			print("Added spell 2 to stack. Size: " + spellStack.Count);
+
+            uiText.text += "Spell 2 \n";
 		}
 		if (Input.GetButtonDown ("Spell_3")) {
 			spellStack.Add(SpellPress.SPELL_3);
 			print("Added spell 3 to stack. Size: " + spellStack.Count);
+
+            uiText.text += "Spell 3 \n";
 		}
 	}
 }
